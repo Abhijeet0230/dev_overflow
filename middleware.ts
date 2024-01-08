6ee309d8eed58ef6ada3f608526e9e8be23b5abf
Abhijeet Singh <singhabhijeet732@gmail.com>
2024-01-09 02:11:38 +0530

    Fixed typos part-5 and renamed webhook to webhooks

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "/api/webhook/",
+    "/api/webhooks(.*)",
     "/question/:id",
     "/tags",
     "/tags/:id",
@@ -12,7 +12,6 @@ export default authMiddleware({
     "/jobs",
   ],
   ignoredRoutes: ["/api/chatgpt"],
-  debug: true,
 });
 
 export const config = {
